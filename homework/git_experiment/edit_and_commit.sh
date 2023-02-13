fdb88cd26e73cd149f895b8698c89daf5b464f8e
graced03 <dxy_03@outlook.com>
2023-02-13 13:15:44 -0500

    submit homework

--- homework/git_experiment/edit_and_commit.sh
+++ homework/git_experiment/edit_and_commit.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-
+module load tex
 for commit_count in {2..100}
 do
   let linenum="10+$commit_count"
-  # Fill in
+  # add a new sentence to foo.tex
+  sed -i "$linenum i This is the first sentence." foo.tex
+  # compile the .tex file
+  pdflatex foo.tex
+  # commit the new files
+  git add -u
+  git commit -m "Add first sentence to TeX file"
 done
